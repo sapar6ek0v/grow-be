386f55532779c7550344abaa1a59af21fbb9c1e1
sapar6ek0v <razetka0309@gmail.com>
2021-10-16 16:08:03 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$('.testimonials__card-boxes').slick({
+    dots: true,
+    centerMode: true,
+    centerPadding: "400",
+    slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
